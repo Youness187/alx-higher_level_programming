09eb9e3ce5909762bb7b61d6f13472cc4c73fb51
younesse el-mouden <dayouness187@gmail.com>
2023-06-08 12:18:11 +0100

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden
+    import hidden_4
 
-    files = dir(hidden)
-    for f in files:
-        if f[0:2] != "__":
-            print(f)
+    names = dir(hidden_4)
+    for name in namess:
+        if name[0:2] != "__":
+            print(name)
