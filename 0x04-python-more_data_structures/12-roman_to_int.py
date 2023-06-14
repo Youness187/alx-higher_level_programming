d785ece503e52ff784d4b89061ab083f5aa71245
younesse el-mouden <dayouness187@gmail.com>
2023-06-14 06:52:04 +0100

    Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    romanN = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    my_sum = 0
+    timp = 0
+    for i in range(len(roman_string)):
+        if romanN[roman_string[i]] <= timp or timp == 0:
+            timp = romanN[roman_string[i]]
+            my_sum += timp
+        else:
+            timp = romanN[roman_string[i]]
+            my_sum = abs(my_sum - timp)
+    return my_sum
