6aa792e4279d7344bb50268fb2a3931382819bb2
younesse el-mouden <dayouness187@gmail.com>
2023-06-27 18:41:34 +0100

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -6,11 +6,13 @@ This module contains an empty class that defines a square.
 Usage Example:
 
     Square = __import__('0-square').Square
-    
+
     my_square = Square()
     print(type(my_square))
     print(my_square.__dict__)
 """
+
+
 class Square:
     """Defines the blueprint of a square"""
 
