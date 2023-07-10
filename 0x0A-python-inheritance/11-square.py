abd90c355026a502a71c251a8107eb0df3d4632d
younesse el-mouden <dayouness187@gmail.com>
2023-07-10 12:39:11 +0100

    quare #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,3 +15,7 @@ class Square(Rectangle):
     def area(self):
         """"returns the area of the square"""
         return self.__size ** 2
+
+    def __str__(self):
+        """informal string reepresentation of the square"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
