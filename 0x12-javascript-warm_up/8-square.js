fbae94a45eb5c0287c53bc3fda28690f6fc80725
younesse el-mouden <dayouness187@gmail.com>
2023-09-11 12:53:16 +0000

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const argv = process.argv;
+const x = parseInt(argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  let c = '';
+  for (let i = 0; i < x; i++) c += 'X';
+  for (let j = 0; j < x; j++) console.log(c);
+}
