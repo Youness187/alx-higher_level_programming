9cf82383d163d7d740b6b08a608853611be31c7c
younesse el-mouden <dayouness187@gmail.com>
2023-09-11 13:50:12 +0000

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,8 +2,7 @@
 const argv = process.argv;
 const argn = parseInt(argv[2]);
 function factorial (num) {
-  if (num > 0) return factorial(num - 1) * num;
-  return 1;
+  return num > 0 ? factorial(num - 1) * num : 1;
 }
 
 console.log(factorial(isNaN(argn) ? 1 : argn));
