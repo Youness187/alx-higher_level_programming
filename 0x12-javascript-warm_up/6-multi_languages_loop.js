1acf40e5ddd03768efd3f8f1592d85c0901c324b
younesse el-mouden <dayouness187@gmail.com>
2023-09-11 12:30:35 +0000

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const lang = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 lang.forEach((e) => console.log(e));
