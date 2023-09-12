68f1faf5caa5435a6155dbc8c83158964b169236
younesse el-mouden <dayouness187@gmail.com>
2023-09-12 16:14:30 +0000

    Square #6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,8 +5,9 @@ class Square extends Rectangle {
     super(size, size);
   }
 
-  charPrint (c = 'X') {
-    for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+  charPrint (c) {
+    if (c === undefined) this.print();
+    else for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
   }
 }
 module.exports = Square;
