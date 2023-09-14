c5d64c5faa3642496da92c4bf41bb00a95097679
younesse el-mouden <dayouness187@gmail.com>
2023-09-14 10:13:04 +0000

    SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+argv = sys.argv
+
+try:
+    db_con = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8",
+    )
+except Exception:
+    sys.exit(0)
+states = db_con.cursor()
+query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id"
+states.execute(query, (argv[4],))
+rows = states.fetchall()
+for row in rows:
+    print(row)
+states.close()
+db_con.close()
