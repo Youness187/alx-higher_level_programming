7536faff28a6d0c65b61b1cc604a2420908d6f88
younesse el-mouden <dayouness187@gmail.com>
2023-09-14 09:43:43 +0000

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+argv = sys.argv
+
+try:
+    db_con = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8",
+    )
+except Exception:
+    sys.exit(0)
+states = db_con.cursor()
+states.execute("SELECT id, name FROM states ORDER BY id")
+rows = states.fetchall()
+for row in rows:
+    if "N" == row[1][0]:
+        print(row)
+states.close()
+db_con.close()
