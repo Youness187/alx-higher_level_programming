5e201252d78502aebf690d544cb223d0cc058da4
younesse el-mouden <dayouness187@gmail.com>
2023-09-14 10:05:56 +0000

    Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+argv = sys.argv
+
+try:
+    db_con = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8",
+    )
+except Exception:
+    sys.exit(0)
+states = db_con.cursor()
+query = "SELECT * FROM states WHERE name = '{}' ORDER BY id".format(argv[4])
+states.execute(query)
+rows = states.fetchall()
+for row in rows:
+    print(row)
+states.close()
+db_con.close()
