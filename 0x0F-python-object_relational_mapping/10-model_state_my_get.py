49b35bffbf1cfb8250bf9bfa0854f1071fe5e78e
younesse el-mouden <dayouness187@gmail.com>
2023-09-14 12:38:58 +0000

    Get a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """
+    lists all State objects from the database hbtn_0e_6_usa
+    """
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter_by(name=sys.argv[4]).all()
+    if len(states) > 0:
+        for state in states:
+            print(state.id)
+    else:
+        print("Not found")
+    session.close()
