d30550dc8f5fe860649f7ac2d3e37b3cc5e34868
younesse el-mouden <dayouness187@gmail.com>
2023-09-29 21:55:13 +0000

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,9 +11,8 @@ if __name__ == "__main__":
     q = ""
     if len(sys.argv) > 1:
         q = sys.argv[1]
-
-    res = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
     try:
+        res = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
         data = res.json()
 
         if data:
@@ -22,5 +21,3 @@ if __name__ == "__main__":
             print("No result")
     except ValueError:
         print("Not a valid JSON")
-    except Exception:
-        print("No result")
