0ec7f5a9142fc62e14d6529ee4067c5d50445ca0
younesse el-mouden <dayouness187@gmail.com>
2023-09-29 20:43:02 +0000

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that fetches https://alx-intranet.hbtn.io/status.
+"""
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        data = response.read()
+        utf8_data = data.decode("utf-8")
+
+    print("Body response:")
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
+    print("\t- utf8 content: {}".format(utf8_data))
