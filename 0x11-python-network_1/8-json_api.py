0e005cc6e61e26e1096e181ad483bc50600053d7
younesse el-mouden <dayouness187@gmail.com>
2023-09-29 21:47:53 +0000

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         if not data:
             print("No result")
         else:
-            print("[{}] {}".format(data["id"], data["name"]))
+            print("[{}] {}".format(data.get("id"), data.get("name")))
 
     except ValueError:
         print("Not a valid JSON")
