66462a728054ad6f2489f3bea669beecfd51b829
younesse el-mouden <dayouness187@gmail.com>
2023-10-24 22:27:36 +0000

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,17 +2,18 @@
 const request = require('request');
 const id = process.argv[2];
 
-const printChar = (characters) => {
-  for (let i = 0; i < characters.length; i++) {
-    request(characters[i], (err, res, body) => {
-      if (err) console.log(err);
-      console.log(JSON.parse(body).name);
-    });
-  }
+const printChar = (characters, i) => {
+  request(characters[i], (err, res, body) => {
+    if (err) console.log(err);
+    console.log(JSON.parse(body).name);
+    if (i + 1 < characters.length) {
+      printChar(characters, i + 1);
+    }
+  });
 };
 
 request(`https://swapi-api.alx-tools.com/api/films/${id}`, async (err, res, body) => {
   if (err) console.log(err);
   const characters = JSON.parse(body).characters;
-  printChar(characters);
+  printChar(characters, 0);
 });
